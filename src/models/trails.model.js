2d84ab047d6488a659bf4d7d587987c755d35f43
thackern1776 <thackern@dukes.jmu.edu>
2020-11-02 14:47:56 -0500

    add trails service

---
+++ src/models/trails.model.js
@@ -0,0 +1,62 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const trails = sequelizeClient.define('trails', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false
+    },
+    trail_name: {
+      type: DataTypes.STRING(25),
+      allowNull: false
+    },
+    difficulty: {
+      type: DataTypes.ENUM('Beginner', 'Intermediate', 'Advanced'),
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    trail_photo: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat_: {
+      type: DataTypes.STRING(11),
+      allowNull: false
+    },
+    long_: {
+      type: DataTypes.STRING(11),
+      allowNull: false
+    },
+    cost: {
+      type: DataTypes.FLOAT,
+      allowNull: true
+    }
+  }, {
+    timestamps: false,
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  trails.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return trails;
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
 const hikers = require('./hikers/hikers.service.js');
+const trails = require('./trails/trails.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(hikers);
+  app.configure(trails);
 }
